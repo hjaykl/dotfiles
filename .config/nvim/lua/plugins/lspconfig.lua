3afed15c3e23b2670609e2b0b523bdc6e9220314
Jacob Poole <114026544+Jacob-mns@users.noreply.github.com>
2025-01-21 20:06:21 +0000

    nvim - refactor LSP setup

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,70 +1,63 @@
-return { {
-	"neovim/nvim-lspconfig",
-	dependencies = {
-		{
-			"folke/lazydev.nvim",
-			ft = "lua", -- only load on lua files
-			opts = {
-				library = {
-					-- See the configuration section for more details
-					-- Load luvit types when the `vim.uv` word is found
-					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+return {
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{
+				"folke/lazydev.nvim",
+				ft = "lua", -- only load on lua files
+				opts = {
+					library = {
+						-- Load luvit types when the `vim.uv` word is found
+						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+					},
 				},
 			},
 		},
-	},
-	config = function()
-		require("lspconfig").lua_ls.setup({})
-		require("lspconfig").ts_ls.setup({})
-		require("lspconfig").eslint.setup({
-			settings = {
-				workingDirectory = { mode = "auto" }, -- Automatically resolve closest ESLint config
-			},
-			root_dir = require("lspconfig.util").root_pattern("project.json", ".eslintrc*", "package.json"),
-			on_attach = function(client, bufnr)
-				-- vim.api.nvim_create_autocmd("BufWritePre", {
-				-- 	buffer = bufnr,
-				-- 	command = "EslintFixAll",
-				-- })
-				vim.api.nvim_buf_set_keymap(
-					bufnr,
-					"n",
-					"<leader>f",
-					"<cmd>EslintFixAll<CR>",
-					{ noremap = true, silent = true }
-				)
+		config = function()
+			-- Centralized on_attach function
+			local on_attach = function(client, bufnr)
+				local opts = { noremap = true, silent = true, buffer = bufnr }
 
-				vim.api.nvim_buf_set_keymap(
-					bufnr,
-					"n",
-					"<leader>ca",
-					"<cmd>lua vim.lsp.buf.code_action()<CR>",
-					{ noremap = true, silent = true }
-				)
-				vim.api.nvim_buf_set_keymap(
-					bufnr,
-					"v",
-					"<leader>ca",
-					"<cmd>lua vim.lsp.buf.range_code_action()<CR>",
-					{ noremap = true, silent = true }
-				)
-			end,
-		})
+				-- Guard against unsupported methods
+				if client.supports_method("textDocument/codeAction") then
+					vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+				end
 
-		vim.api.nvim_create_autocmd('LspAttach', {
-			callback = function(args)
-				local client = vim.lsp.get_client_by_id(args.data.client_id)
-				if not client then return end
-				-- if client.supports_method('textDocument/formatting') then
-				-- 	-- Create a keymap for vim.lsp.buf.rename()
-				-- 	vim.api.nvim_create_autocmd('BufWritePre', {
-				-- 		buffer = args.buf,
-				-- 		callback = function()
-				-- 			vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-				-- 		end
-				-- 	})
-				-- end
-			end,
-		})
-	end
-} }
+				if client.supports_method("textDocument/definition") then
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+				end
+
+				if client.supports_method("textDocument/implementation") then
+					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+				end
+
+				if client.supports_method("textDocument/rename") then
+					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+				end
+
+				-- Diagnostic navigation
+				vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+				vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+			end
+
+			-- Set up LSP servers
+			require("lspconfig").lua_ls.setup({
+				on_attach = on_attach,
+			})
+
+			require("lspconfig").ts_ls.setup({
+				on_attach = on_attach,
+			})
+
+			require("lspconfig").eslint.setup({
+				on_attach = on_attach,
+				settings = {
+					workingDirectory = { mode = "auto" }, -- Automatically resolve closest ESLint config
+				},
+				root_dir = require("lspconfig.util").root_pattern("project.json", ".eslintrc*", "package.json"),
+			})
+
+			-- Optional: Add more LSP server setups here with the same on_attach
+		end,
+	}
+}
