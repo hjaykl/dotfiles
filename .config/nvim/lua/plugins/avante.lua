114a69cb692ee3f39dd4524f3cb1085de467933e
Jacob Poole <4a61636f62@proton.me>
2025-01-21 20:02:36 +0000

    nvim - switch avante to claude

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -1,48 +1,48 @@
 return {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = false, -- Always load eagerly
-    version = false, -- Set to false for the latest code
-    opts = {
-        provider = "openai", -- Example configuration
-        api_key = vim.env.OPENAI_API_KEY, -- Use an environment variable for the API key
-        -- Add additional configuration options here
-    },
-    build = "make", -- Build Avante if necessary
-    dependencies = {
-        -- Required dependencies
-        "stevearc/dressing.nvim",
-        "nvim-lua/plenary.nvim",
-        "MunifTanjim/nui.nvim",
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  lazy = false,                          -- Always load eagerly
+  version = false,                       -- Set to false for the latest code
+  opts = {
+    provider = "claude",                 -- Example configuration
+    api_key = vim.env.ANTHROPIC_API_KEY, -- Use an environment variable for the API key
+    -- Add additional configuration options here
+  },
+  build = "make", -- Build Avante if necessary
+  dependencies = {
+    -- Required dependencies
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
 
-        -- Optional dependencies
-        "hrsh7th/nvim-cmp", -- Autocompletion for Avante commands and mentions
-        "nvim-tree/nvim-web-devicons", -- Icons
-        "zbirenbaum/copilot.lua", -- For providers='copilot'
+    -- Optional dependencies
+    "hrsh7th/nvim-cmp",            -- Autocompletion for Avante commands and mentions
+    "nvim-tree/nvim-web-devicons", -- Icons
+    "zbirenbaum/copilot.lua",      -- For providers='copilot'
 
-        -- Image pasting support
-        {
-            "HakonHarnes/img-clip.nvim",
-            event = "VeryLazy",
-            opts = {
-                default = {
-                    embed_image_as_base64 = false,
-                    prompt_for_file_name = false,
-                    drag_and_drop = {
-                        insert_mode = true,
-                    },
-                    use_absolute_path = true, -- Required for Windows
-                },
-            },
+    -- Image pasting support
+    {
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          use_absolute_path = true, -- Required for Windows
         },
-
-        -- Markdown rendering support
-        {
-            "MeanderingProgrammer/render-markdown.nvim",
-            opts = {
-                file_types = { "markdown", "Avante" },
-            },
-            ft = { "markdown", "Avante" },
-        },
+      },
     },
+
+    -- Markdown rendering support
+    {
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
 }
