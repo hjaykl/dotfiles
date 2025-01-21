38b103bb6f10dbe4463c2158c6b4555186652d50
Jacob Poole <114026544+Jacob-mns@users.noreply.github.com>
2025-01-21 20:06:33 +0000

    nvim - transparent background

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -1,10 +1,13 @@
 return {
-  "folke/tokyonight.nvim",
-  config = function() 
-	  vim.cmd.colorscheme "tokyonight"
-  end,
-  lazy = false,
-  priority = 1000,
-  opts = {},
+	"folke/tokyonight.nvim",
+	config = function()
+		local colors = require("tokyonight").setup({
+			transparent = true
+		})
+		vim.cmd.colorscheme "tokyonight"
+	end,
+	lazy = false,
+	priority = 1000,
+	opts = {},
 
 }
