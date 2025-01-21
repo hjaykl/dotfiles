703ca6b464e5bdaf3cb9a6f421c044c517b0b4cf
Jacob Poole <114026544+Jacob-mns@users.noreply.github.com>
2025-01-21 18:19:47 +0000

    nvim - mini.files keybinds

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,92 +1,96 @@
 return { {
-  'echasnovski/mini.nvim',
-  version = false,
-  config = function()
-    local tabline = require 'mini.tabline'
-    -- Keybindings for Mini.files
-    vim.keymap.set('n', '<Leader>e', ':lua MiniFiles.open()<CR>', { desc = 'Open MiniFiles' })
-    tabline.setup()
+	'echasnovski/mini.nvim',
+	version = false,
+	config = function()
+		local tabline = require 'mini.tabline'
+		-- Keybindings for Mini.files
+		vim.keymap.set('n', '<Leader>e', ':lua MiniFiles.open()<CR>', { desc = 'Open MiniFiles' })
+		tabline.setup()
 
-    local statusline = require 'mini.statusline'
-    statusline.setup()
+		local statusline = require 'mini.statusline'
+		statusline.setup()
 
-    local icons = require 'mini.icons'
-    icons.setup()
+		local icons = require 'mini.icons'
+		icons.setup()
 
-    local pick = require 'mini.pick'
-    pick.setup()
+		local pick = require 'mini.pick'
+		pick.setup()
 
-    -- Keybindings for Mini.pick
-    vim.keymap.set('n', '<Leader>pf', ':Pick files<CR>', { desc = 'Pick files' })
-    vim.keymap.set('n', '<Leader>pb', ':Pick buffers<CR>', { desc = 'Pick buffers' })
-    vim.keymap.set('n', '<Leader>pg', ':Pick grep_live<CR>', { desc = 'Pick live grep' })
+		-- Keybindings for Mini.pick
+		vim.keymap.set('n', '<Leader>pf', ':Pick files<CR>', { desc = 'Pick files' })
+		vim.keymap.set('n', '<Leader>pb', ':Pick buffers<CR>', { desc = 'Pick buffers' })
+		vim.keymap.set('n', '<Leader>pg', ':Pick grep_live<CR>', { desc = 'Pick live grep' })
 
-    local files = require 'mini.files'
-    files.setup()
+		local files = require 'mini.files'
+		files.setup()
 
-    -- Keybindings for Mini.files
-    vim.keymap.set('n', '<Leader>e', ':lua MiniFiles.open()<CR>', { desc = 'Open MiniFiles' })
+		-- Keybindings for Mini.files
+		-- Keybinding to open at the current file's directory
+		vim.keymap.set('n', '<Leader>e', function()
+			require('mini.files').open(vim.fn.expand('%:p:h')) -- Open mini.files at current file's directory
+		end, { desc = 'Open MiniFiles at current file directory' })
+		vim.keymap.set('n', '<Leader>E', ':lua MiniFiles.open()<CR>', { desc = 'Open MiniFiles' })
 
-    -- Bind Escape to close MiniFiles
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = 'minifiles', -- Trigger only for MiniFiles
-      callback = function()
-        vim.keymap.set('n', '<Esc>', function()
-          files.close()
-        end, { buffer = true, desc = 'Close MiniFiles picker' })
-      end,
-    })
+		-- Bind Escape to close MiniFiles
+		vim.api.nvim_create_autocmd('FileType', {
+			pattern = 'minifiles', -- Trigger only for MiniFiles
+			callback = function()
+				vim.keymap.set('n', '<Esc>', function()
+					files.close()
+				end, { buffer = true, desc = 'Close MiniFiles picker' })
+			end,
+		})
 
-    local comment = require 'mini.comment'
-    comment.setup()
+		local comment = require 'mini.comment'
+		comment.setup()
 
-    local completion = require 'mini.completion'
-    completion.setup()
+		local completion = require 'mini.completion'
+		completion.setup()
 
-    local miniclue = require('mini.clue')
-    miniclue.setup({
-      triggers = {
-        -- Leader triggers
-        { mode = 'n', keys = '<Leader>' },
-        { mode = 'x', keys = '<Leader>' },
+		local miniclue = require('mini.clue')
+		miniclue.setup({
+			triggers = {
+				-- Leader triggers
+				{ mode = 'n', keys = '<Leader>' },
+				{ mode = 'x', keys = '<Leader>' },
 
-        -- Built-in completion
-        { mode = 'i', keys = '<C-x>' },
+				-- Built-in completion
+				{ mode = 'i', keys = '<C-x>' },
 
-        -- `g` key
-        { mode = 'n', keys = 'g' },
-        { mode = 'x', keys = 'g' },
+				-- `g` key
+				{ mode = 'n', keys = 'g' },
+				{ mode = 'x', keys = 'g' },
 
-        -- Marks
-        { mode = 'n', keys = "'" },
-        { mode = 'n', keys = '`' },
-        { mode = 'x', keys = "'" },
-        { mode = 'x', keys = '`' },
+				-- Marks
+				{ mode = 'n', keys = "'" },
+				{ mode = 'n', keys = '`' },
+				{ mode = 'x', keys = "'" },
+				{ mode = 'x', keys = '`' },
 
-        -- Registers
-        { mode = 'n', keys = '"' },
-        { mode = 'x', keys = '"' },
-        { mode = 'i', keys = '<C-r>' },
-        { mode = 'c', keys = '<C-r>' },
+				-- Registers
+				{ mode = 'n', keys = '"' },
+				{ mode = 'x', keys = '"' },
+				{ mode = 'i', keys = '<C-r>' },
+				{ mode = 'c', keys = '<C-r>' },
 
-        -- Window commands
-        { mode = 'n', keys = '<C-w>' },
+				-- Window commands
+				{ mode = 'n', keys = '<C-w>' },
 
-        -- `z` key
-        { mode = 'n', keys = 'z' },
-        { mode = 'x', keys = 'z' },
-      },
+				-- `z` key
+				{ mode = 'n', keys = 'z' },
+				{ mode = 'x', keys = 'z' },
+			},
 
-      clues = {
-        -- Enhance this by adding descriptions for <Leader> mapping groups
-        miniclue.gen_clues.builtin_completion(),
-        miniclue.gen_clues.g(),
-        miniclue.gen_clues.marks(),
-        miniclue.gen_clues.registers(),
-        miniclue.gen_clues.windows(),
-        miniclue.gen_clues.z(),
-      },
-    })
-  end
+			clues = {
+				-- Enhance this by adding descriptions for <Leader> mapping groups
+				miniclue.gen_clues.builtin_completion(),
+				miniclue.gen_clues.g(),
+				miniclue.gen_clues.marks(),
+				miniclue.gen_clues.registers(),
+				miniclue.gen_clues.windows(),
+				miniclue.gen_clues.z(),
+			},
+		})
+	end
 },
 }
