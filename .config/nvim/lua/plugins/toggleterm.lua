ee64d0f512fcb6d63eb3b7e58b64346b6eaea869
Jacob Poole <4a61636f62@proton.me>
2025-02-08 12:14:24 +0000

    nvim - toggleterm & lazygit integration

---
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,44 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = "*",
+  config = function()
+    require("toggleterm").setup({
+      size = function(term)
+        if term.direction == "horizontal" then
+          return 15
+        elseif term.direction == "vertical" then
+          return vim.o.columns * 0.4
+        end
+      end,
+      float_opts = {
+        border = "curved",
+        width = function()
+          return math.floor(vim.o.columns * 0.9)
+        end,
+        height = function()
+          return math.floor(vim.o.lines * 0.9)
+        end,
+      },
+    })
+
+    local Terminal = require('toggleterm.terminal').Terminal
+    local lazygit = Terminal:new({
+      cmd = "lazygit",
+      dir = "git_dir",
+      direction = "float",
+      float_opts = {
+        border = "curved",
+      },
+      on_open = function(term)
+        vim.cmd("startinsert!")
+        vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+      end,
+    })
+
+    function _lazygit_toggle()
+      lazygit:toggle()
+    end
+
+    vim.keymap.set("n", "<leader>gg", "<cmd>lua _lazygit_toggle()<CR>", { desc = "Lazygit" })
+  end,
+}
