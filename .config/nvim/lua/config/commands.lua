90eb820e897d951b498f6dfdf8777606d6f5244b
Jacob Poole <jacob.poole@octopus.energy>
2026-01-21 22:39:22 +0000

    nvim - command to reload config

---
+++ .config/nvim/lua/config/commands.lua
@@ -0,0 +1,9 @@
+vim.api.nvim_create_user_command('ReloadConfig', function()
+  for key, _ in pairs(package.loaded) do
+    if key:match('^config') or key:match('^plugins') then
+      package.loaded[key] = nil
+    end
+  end
+  vim.cmd('luafile ' .. vim.fn.stdpath('config') .. '/init.lua')
+  vim.notify("Neovim config reloaded!", vim.log.levels.INFO)
+end, {desc = 'Reload Neovim configuration'})
