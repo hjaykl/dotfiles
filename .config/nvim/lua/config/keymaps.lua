50a2a12d4eb1996b156ea0244c8d517804e31d29
Jacob Poole <jacob.poole@octopus.energy>
2026-01-21 22:39:49 +0000

    nvim - terminal keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -7,3 +7,8 @@ map("n", "<leader>-", "<C-W>s", { desc = "Split Window Below", remap = true })
 map("n", "<leader>|", "<C-W>v", { desc = "Split Window Right", remap = true })
 map("n", "<leader>wd", "<C-W>c", { desc = "Delete Window", remap = true })
 map('n', '<BS>', '<C-^>', { desc = 'Switch to alternate buffer' })
+map('t', '<C-w>', '<C-\\><C-n><C-w>', { desc = 'Window commands from terminal', noremap = true, silent = true })
+
+map('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode', noremap = true, silent = true })
+map('t', '<C-e>', '<Esc>', { desc = 'Send Esc to terminal', noremap = true, silent = true })
+
