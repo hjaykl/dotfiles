d9aa73fb81426c9f36d161af53e9669731c68a6f
Jacob Poole <jacob.poole@octopus.energy>
2026-01-22 09:44:52 +0000

    nvim - keymap to toggle qf list

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -12,3 +12,19 @@ map('t', '<C-w>', '<C-\\><C-n><C-w>', { desc = 'Window commands from terminal', 
 map('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode', noremap = true, silent = true })
 map('t', '<C-e>', '<Esc>', { desc = 'Send Esc to terminal', noremap = true, silent = true })
 
+local function toggle_qf()
+  local qf_exists = false
+  for _, win in pairs(vim.fn.getwininfo()) do
+    if win["quickfix"] == 1 then
+      qf_exists = true
+    end
+  end
+  if qf_exists then
+    vim.cmd("cclose")
+  else
+    vim.cmd("copen")
+  end
+end
+
+map("n", "<leader>qq", toggle_qf, { desc = "Toggle Quickfix List" })
+
