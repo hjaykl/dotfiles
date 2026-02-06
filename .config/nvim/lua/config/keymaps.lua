426e5a129eb9ce975c6b4fcc89f3c99ba8625147
Jacob Poole <contact@jacobpoole.uk>
2026-02-06 17:51:21 +0000

    nvim - change ai helper keybinds

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,13 +3,13 @@ local map = vim.keymap.set
 map('n', '<leader>pc', ':let @+ = expand("%:p")<CR>',
   { desc = "Copy filepath to clipboard", noremap = true, silent = true })
 
-map('n', '<leader>cf', function()
+map('n', '<leader>af', function()
   local filepath = vim.fn.expand('%:p')
   vim.fn.setreg('+', '@' .. filepath)
   vim.notify('Copied: @' .. filepath, vim.log.levels.INFO)
 end, { desc = "Copy @ reference to clipboard", noremap = true, silent = true })
 
-map('n', '<leader>ct', function()
+map('n', '<leader>at', function()
   local filepath = vim.fn.expand('%:p')
   local line = vim.fn.line('.')
   local ref = '@' .. filepath .. ':L' .. line
@@ -17,7 +17,7 @@ map('n', '<leader>ct', function()
   vim.notify('Copied: ' .. ref, vim.log.levels.INFO)
 end, { desc = "Copy @ reference with line to clipboard", noremap = true, silent = true })
 
-map('v', '<leader>ct', function()
+map('v', '<leader>at', function()
   local filepath = vim.fn.expand('%:p')
   local start_line = vim.fn.line('v')
   local end_line = vim.fn.line('.')
@@ -43,7 +43,7 @@ local function open_or_focus_qf()
       break
     end
   end
-  
+
   if qf_winid then
     vim.fn.win_gotoid(qf_winid)
   else
@@ -57,7 +57,7 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "qf",
   callback = function()
     vim.keymap.set("n", "q", ":cclose<CR>", { buffer = true, desc = "Close Quickfix" })
-    
+
     local function delete_qf_items(start_line, end_line)
       local qf_list = vim.fn.getqflist()
       for i = end_line, start_line, -1 do
@@ -67,13 +67,13 @@ vim.api.nvim_create_autocmd("FileType", {
       vim.cmd("copen")
       vim.fn.cursor(math.min(start_line, #qf_list), 1)
     end
-    
+
     vim.keymap.set("n", "dd", function()
       local line = vim.fn.line('.')
       local count = vim.v.count1
       delete_qf_items(line, line + count - 1)
     end, { buffer = true, desc = "Delete quickfix item(s)" })
-    
+
     vim.keymap.set("x", "d", function()
       local start_line = vim.fn.line('v')
       local end_line = vim.fn.line('.')
@@ -84,4 +84,3 @@ vim.api.nvim_create_autocmd("FileType", {
     end, { buffer = true, desc = "Delete quickfix items" })
   end,
 })
-
