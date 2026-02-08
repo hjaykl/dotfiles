b1b8abdf220b85de468427be6f747d03a28ef50a
Jacob Poole <contact@jacobpoole.uk>
2026-02-08 16:43:01 +0000

    nvim - alleyoop plugin

--- .config/nvim/lua/plugins/alleyoop.lua
+++ .config/nvim/lua/plugins/alleyoop.lua
@@ -1,4 +1,9 @@
 MiniDeps.later(function()
-  vim.opt.rtp:prepend(vim.fn.expand("~/Dev/alleyoop"))
+  local dev_path = vim.fn.expand("~/Dev/alleyoop")
+  if vim.uv.fs_stat(dev_path) then
+    vim.opt.rtp:prepend(dev_path)
+  else
+    MiniDeps.add({ source = "hjaykl/alleyoop.nvim" })
+  end
   require("alleyoop").setup()
 end)
